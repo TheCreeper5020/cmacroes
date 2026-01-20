860f6f5589818cf2ee00631ab7e70f27f3a69c31
TheCreeper5020 <thecreeper5020@gmail.com>
2026-01-19 19:00:37 -0600

    Fix header guard and include assert.h

--- unimpl.h
+++ unimpl.h
@@ -1,6 +1,11 @@
-#ifndef unimplemented
+#ifndef UNIMPL_H
+#define UNIMPL_H
+
+#include <assert.h>
+
 #define unimplemented() do { \
     fprintf(stderr, "%s(): Function not implemented.\n", __func__);\
     abort();\
 } while (0)
+
 #endif
