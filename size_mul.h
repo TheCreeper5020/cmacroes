7050e206ee59ed7f8e7888f6197a1c4f02930f50
TheCreeper5020 <thecreeper5020@gmail.com>
2026-02-22 15:35:13 -0600

    Add size_mul.h for size definitions in bytes

---
+++ size_mul.h
@@ -0,0 +1,16 @@
+#ifndef SIZE_MUL_H
+#define SIZE_MUL_H
+
+#define B 1ull
+#define KB (1000ull * B)
+#define KiB (1024ull * B)
+#define MB (1000ull * KB)
+#define MiB (1024ull * KiB)
+#define GB (1000ull * MB)
+#define GiB (1024ull * MiB)
+#define TB (1000ull * GB)
+#define TiB (1024ull * GiB)
+#define PB (1000ull * TB)
+#define PiB (1024ull * TiB)
+
+#endif
