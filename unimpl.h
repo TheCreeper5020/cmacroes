79acbbf3e94e4e8fad5b509f758ceb99826909dd
TheCreeper5020 <thecreeper5020@gmail.com>
2026-04-19 08:43:08 -0500

    Remove C++ specific implementation from unimpl.h

--- unimpl.h
+++ unimpl.h
@@ -1,18 +1,6 @@
 #ifndef UNIMPL_H
 #define UNIMPL_H
 
-#ifdef __cplusplus
-
-#include <cstdlib>
-#include <cstdio>
-
-#define unimplemented() do { \
-    std::fprintf(stderr, "%s(): Function not implemented (line %d in file %s).\n", __func__, __LINE__, __FILE__);\
-    std::abort();\
-} while (0)
-
-#else
-
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -20,7 +8,5 @@
     fprintf(stderr, "%s(): Function not implemented (line %d in file %s).\n", __func__, __LINE__, __FILE__);\
     abort();\
 } while (0)
-
-#endif
 
 #endif
