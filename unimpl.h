4da85810675ac663a8bba86dc3f530c4f75922e9
TheCreeper5020 <thecreeper5020@gmail.com>
2026-04-19 08:40:05 -0500

    Enhance unimplemented macro with line and file info

--- unimpl.h
+++ unimpl.h
@@ -1,12 +1,26 @@
 #ifndef UNIMPL_H
 #define UNIMPL_H
 
+#ifdef __cplusplus
+
+#include <cstdlib>
+#include <cstdio>
+
+#define unimplemented() do { \
+    std::fprintf(stderr, "%s(): Function not implemented (line %d in file %s).\n", __func__, __LINE__, __FILE__);\
+    std::abort();\
+} while (0)
+
+#else
+
 #include <stdlib.h>
 #include <stdio.h>
 
 #define unimplemented() do { \
-    fprintf(stderr, "%s(): Function not implemented.\n", __func__);\
+    fprintf(stderr, "%s(): Function not implemented (line %d in file %s).\n", __func__, __LINE__, __FILE__);\
     abort();\
 } while (0)
+
+#endif
 
 #endif
