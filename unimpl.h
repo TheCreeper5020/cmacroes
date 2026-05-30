38382a9a9949b3e3c6501b3fac708558655b0e28
TheCreeper5020 <thecreeper5020@gmail.com>
2026-05-29 19:44:16 -0500

    Update unimpl.h
    Add a TODO macro so that we can seperate temporary, not-yet-implemented logic from permanently unimplemented logic

--- unimpl.h
+++ unimpl.h
@@ -4,9 +4,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define unimplemented() do { \
+#define unimplemented() do {\
     fprintf(stderr, "%s(): Function not implemented (line %d in file %s).\n", __func__, __LINE__, __FILE__);\
     abort();\
 } while (0)
+
+#define todo() do {\
+    fprintf(stderr, "TODO hit on line %d in function %s() in file %s", __LINE__, __func__, __FILE__);\
+    abort();\
+} while (0);
 
 #endif
